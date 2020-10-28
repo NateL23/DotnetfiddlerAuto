b8763e37cbf3fa38c8f60affdc3b48eb3cba2bdc
XneveroddorevenX <mountandoman@gmail.com>
2020-10-27 21:20:27 -0400

    Test 1 Passing

---
+++ DNFAutoProject/Extensions/WebElementExtensions.cs
@@ -0,0 +1,36 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace DNFAutoFramework.Extensions
+{
+    public static class WebElementExtensions
+    {
+        public static void SelectDropdownList(this IWebElement element, string value)
+        {
+            SelectElement ddl = new SelectElement(element);
+            ddl.SelectByText(value);
+        }
+        
+        public static void AssertElementPresent(this IWebElement element)
+        {
+            if(!IsElementPresent(element))
+            {
+                throw new Exception(string.Format("Err: Assert Element Text failed"));
+            };
+        }
+
+        private static bool IsElementPresent(IWebElement element)
+        {
+            try
+            {
+                bool ele = element.Displayed;
+                return true;
+            }
+            catch(Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
